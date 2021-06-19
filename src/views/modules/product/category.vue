a33d021c0db968e7e1de6bde416be823f28537bf
tuqi.stjj <situjunjie@come-future.com>
2021-06-19 20:16:49 +0800

    首次构建

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="category-list">
+    <!--这是一棵树-->
+    <el-tree
+      :data="treeData"
+      props="{label:'',children:''}"
+      @node-click="handleNodeClick"
+    ></el-tree>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      treeData: [],
+    };
+  },
+  created(){
+      this.initTreeData()
+  },
+  methods: {
+    handleNodeClick() {
+      console.log("handleNodeClick");
+    },
+    initTreeData(){
+        this.$http({
+          url: this.$http.adornUrl('product/category/category/list/tree'),
+          method: 'get'
+        }).then(({data}) => {
+          if (data && data.code === 0) {
+              this.treeData = data.category
+          } else {
+
+          }
+        })
+    }
+  },
+};
+</script>
